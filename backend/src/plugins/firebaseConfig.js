702a019bfe76511a93cf56499a9076b520d5b62a
Joni Lillsund <126078929+jilwar25@users.noreply.github.com>
2024-08-17 22:59:20 +0300

    Delete sensitive file

--- backend/src/plugins/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: "AIzaSyDzVzCFGlNkkc0mdtGZosh7Zg5zDIzEtks",
-  authDomain: "nahkakauppa-74e44.firebaseapp.com",
-  projectId: "nahkakauppa-74e44",
-  storageBucket: "nahkakauppa-74e44.appspot.com",
-  messagingSenderId: "169300472765",
-  appId: "1:169300472765:web:aa3eb5ec4d15d60ddd18d4"
-};
-
